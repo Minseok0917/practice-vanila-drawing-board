188542b765a8ea929d35923563ae73dcd8a99ac6
Minseok Jung <37059223+Minseok0917@users.noreply.github.com>
2024-01-10 16:56:06 +0900

    feating(PVD-7) : 툴팁 선택 이벤트 추가(1-touch, 2-selected, 3-rect, 4-circle)

--- js/common.js
+++ js/common.js
@@ -1,8 +1,52 @@
+import { Canvas } from "./module.canvas.js";
 const $app = document.getElementById("app");
-const $canvas = document.createElement("canvas");
-const context = $canvas.getContext("2d");
-$app.append($canvas);
 
-context.beginPath();
-context.rect(0, 0, 100, 100);
-context.fill();
+class DrawingBoard extends Canvas {
+  static #instance;
+  #mode = "touch"; // touch | selected | shape-rect | shape-circle
+
+  constructor() {
+    super();
+    if (!DrawingBoard.#instance) DrawingBoard.#instance = this;
+    this.setCanvasRect(window.innerWidth, window.innerHeight);
+    this.#handlerManagment();
+    return DrawingBoard.#instance;
+  }
+  static getInstance() {
+    if (!DrawingBoard.#instance) DrawingBoard.#instance = new DrawingBoard();
+    return DrawingBoard.#instance;
+  }
+
+  // handlers
+  #handlerManagment() {
+    this.event("mousedown", this.#mousedownHandler)
+      .event("mousemove", this.#mousemoveHandler)
+      .event("mouseup", this.#mouseupHandler);
+    document.addEventListener("keydown", this.#documentKeydownHandler);
+    window.addEventListener("resize", this.#windowResizeHandler);
+  }
+  #documentKeydownHandler(event) {
+    switch (event.key) {
+      case "1":
+        this.#mode = "touch";
+        break;
+      case "2":
+        this.#mode = "selected";
+        break;
+      case "3":
+        this.#mode = "shape-rect";
+        break;
+      case "4":
+        this.#mode = "shape-circle";
+        break;
+    }
+  }
+  #windowResizeHandler() {
+    this.setCanvasRect(window.innerWidth, window.innerHeight);
+  }
+  #mousedownHandler(event) {}
+  #mousemoveHandler(event) {}
+  #mouseupHandler(event) {}
+}
+
+const drawingBoardInstance = DrawingBoard.getInstance().mount($app);
---
+++ js/module.canvas.js
@@ -0,0 +1,46 @@
+export class Canvas {
+  #$canvas;
+  #context;
+  #canvasWidth = 0;
+  #canvasHeight = 0;
+  #handlers = new Map();
+  constructor() {
+    this.#$canvas = document.createElement("canvas");
+    this.#context = this.#$canvas.getContext("2d");
+  }
+
+  setCanvasRect(width, height) {
+    this.#setCanvasWidth(width);
+    this.#setCanvasHeight(height);
+    return this;
+  }
+
+  #setCanvasWidth(width) {
+    this.#canvasWidth = width;
+    this.#$canvas.width = width;
+  }
+
+  #setCanvasHeight(height) {
+    this.#canvasHeight = height;
+    this.#$canvas.height = height;
+  }
+
+  mount($parentElement) {
+    $parentElement.append(this.#$canvas);
+    return this;
+  }
+
+  event(key, callback) {
+    if (this.#handlers.has(key)) throw new Error(`이미 중복된 이벤트가 존재합니다 ${key}`);
+    this.#handlers.set(key, callback);
+    this.#$canvas.addEventListener(key, callback);
+    return this;
+  }
+
+  get $canvas() {
+    return this.#$canvas;
+  }
+  get context() {
+    return this.#context;
+  }
+}
