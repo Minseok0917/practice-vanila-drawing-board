4a30b78793f47d3caa39d76ecad261f3a4501fb7
Minseok Jung <37059223+Minseok0917@users.noreply.github.com>
2024-01-16 15:39:50 +0900

    refacotring(PVD-16) : 새로운 시작

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-import { Excalidraw } from "./excalidraw/app.js";
+import { Excalidraw } from "./#unrefactoring_excalidraw-20240116/excalidraw/app.js";
 
 const $app = document.getElementById("app");
 new Excalidraw().mount($app);
